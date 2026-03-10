7776b22a03895652908ea3fdaa132b63d93e0494
evertontessari <54908471+evertontessari@users.noreply.github.com>
2026-03-10 09:41:55 -0300

    chore: configurar supabase e atualizar build 20260310-3

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 window.CLOUD_CONFIG = {
-  enabled: false,
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  enabled: true,
+  supabaseUrl: "https://uzsginewuhcommjahviw.supabase.co",
+  supabaseAnonKey: "sb_publishable_h5X6AD4-bDDkTOCedpcs5g_0ymNyqd1",
   listId: "apartamento-marjana-everton",
 };
